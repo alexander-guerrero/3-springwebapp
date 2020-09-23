bd29b8692f25f80cda96dbdf9373855843c1e619
Alexander Guerrero <alex.guelu@gmail.com>
2020-09-23 00:40:28 -0500

    033 La capa de Servicio

--- src/com/alex/controller/AdminController.java
+++ src/com/alex/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.alex.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.alex.pojo.Admin;
+import com.alex.service.AdminService;
 
 @Controller
 public class AdminController {
+	
+	@Autowired
+	private AdminService adminService;
 
 	// Si no se indica el método HTTP, por defecto asume el request de tipo GET
 	@RequestMapping("/admin")
---
+++ src/com/alex/service/AdminService.java
@@ -0,0 +1,14 @@
+package com.alex.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alex.dao.AdminDao;
+
+@Service
+public class AdminService {
+	
+	@Autowired
+	private AdminDao adminDao;
+	
+}
